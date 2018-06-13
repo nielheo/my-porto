fd89e3d98fd4358f50b19169dd406d78f38eae53
daniel <daniel.hartono@chilindo.com>
2018-06-13 14:36:35 +0700

    sample from react-native-navigation

--- my_porto_native/index.js
+++ my_porto_native/index.js
@@ -1,22 +1,57 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import App from './App';
+import {Platform} from 'react-native';
+import {Navigation} from 'react-native-navigation';
+import {registerScreens, registerScreenVisibilityListener} from './src/screens';
 
-//import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
 
-const Client = () => {
-  /*const networkInterface = createNetworkInterface({
-    uri: 'http://52.221.195.25:3000/graphql'
-  })*/
-  const client = new ApolloClient({
-    uri: "http://52.221.195.25:3000/graphql"
+// screen related book keeping
+registerScreens();
+registerScreenVisibilityListener();
+
+const tabs = [{
+  label: 'Actions',
+  screen: 'example.Actions',
+  icon: require('./img/swap.png'),
+  title: 'Navigation Actions',
+},{
+  label: 'Navigation',
+  screen: 'example.Types',
+  icon: require('./img/list.png'),
+  title: 'Navigation Types',
+}, ];
+
+if (Platform.OS === 'android') {
+  tabs.push({
+    label: 'Transitions',
+    screen: 'example.Transitions',
+    icon: require('./img/transform.png'),
+    title: 'Navigation Transitions',
   });
-  return (
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>)
 }
 
-AppRegistry.registerComponent('my_porto_native', () => Client);
+// this will start our app
+Navigation.startTabBasedApp({
+  tabs,
+  animationType: Platform.OS === 'ios' ? 'slide-down' : 'fade',
+  tabsStyle: {
+    tabBarBackgroundColor: '#003a66',
+    tabBarButtonColor: '#ffffff',
+    tabBarSelectedButtonColor: '#ff505c',
+    tabFontFamily: 'BioRhyme-Bold',
+  },
+  appStyle: {
+    tabBarBackgroundColor: '#003a66',
+    navBarButtonColor: '#ffffff',
+    tabBarButtonColor: '#ffffff',
+    navBarTextColor: '#ffffff',
+    tabBarSelectedButtonColor: '#ff505c',
+    navigationBarColor: '#003a66',
+    navBarBackgroundColor: '#003a66',
+    statusBarColor: '#002b4c',
+    tabFontFamily: 'BioRhyme-Bold',
+  },
+  drawer: {
+    left: {
+      screen: 'example.Types.Drawer'
+    }
+  }
+});
